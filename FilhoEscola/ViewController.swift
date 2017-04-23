802a48a82cf276f3c549251f8c59ac78298955fa
pascke <everton.pascke@gmail.com>
2017-04-23 20:38:24 -0300

    Initial Commit

---
+++ FilhoEscola/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FilhoEscola
+//
+//  Created by Everton Luiz Pascke on 23/04/17.
+//  Copyright © 2017 Wasys Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
